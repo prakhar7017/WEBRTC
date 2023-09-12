86b0d9c705c87fc1be0b1f17636a84ac90c4049a
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2023-09-13 03:03:05 +0530

    added socket connections

--- server/app.js
+++ server/app.js
@@ -2,3 +2,19 @@ const {Server}=require("socket.io");
 const io=new Server(8000,{
     cors: true
 });
+
+io.on("connection",(socket)=>{
+    socket.emit("user",socket.id);
+
+    socket.on("disconnect",(reason)=>{
+        socket.broadcast.emit("user-disconnected",socket.id);
+    })
+
+    socket.on("first:called",(to,from,callerName,signalData)=>{
+        socket.to(to).emit("second:ack",{from,callerName,signalData});
+    })
+
+    socket.on("second:call-accepted",(data)=>{
+        socket.to(data.to).emit("fisrt:ack",data.signalData);
+    });
+})
