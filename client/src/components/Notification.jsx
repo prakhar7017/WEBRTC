4134de554a10a9a10e614ee37659e62a69813382
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2023-09-14 01:19:43 +0530

    added notification page

--- client/src/components/Notification.jsx
+++ client/src/components/Notification.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Button } from '@mui/material'
+import { SocketContext } from './ProvideContext'
 
 const Notification = () => {
+  const {answerCall,call,callAccepted}=useContext(SocketContext);
+
   return (
-    <div>Notification</div>
+    <>
+      {call.isReceivedCall && !callAccepted && (
+        <div style={{display:'flex', justifyContent:'center'}}>
+          <h1>{call.name} is calling</h1>
+          <Button variant="contained" color="primary" onClick={answerCall}>Answer</Button>
+        </div>
+      )}
+    </>
   )
 }
 
