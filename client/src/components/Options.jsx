2a7e8cbb470a624b52290978f3ba35f505870a91
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2023-09-14 01:11:30 +0530

    added videoPlayer page and option page

--- client/src/components/Options.jsx
+++ client/src/components/Options.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { Children, useContext,useState } from 'react'
+import {Button,TextField,Grid,Typography,Container,Paper} from "@mui/material"
+import {CopyToClipboard} from "react-copy-to-clipboard"
+import {Assignment,Phone,PhoneDisabled} from "@mui/icons-material"  
 
-const Options = () => {
+import { SocketContext } from './ProvideContext'
+
+const Options = ({children}) => {
+  const {myself,callAccepted,name,setName,callEnded,leaveCall,callUser} =useContext(SocketContext);
+  const [idToCall,setIdToCall]=useState("");
   return (
-    <div>Options</div>
+    <Container>
+      <Paper elevation={10}>
+      <form>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
+            <Typography variant="h6" gutterBottom> Account Info</Typography>
+            <TextField label="Name" value={name} onChange={(e)=>setName(e.target.value)} fullWidth/>
+            <CopyToClipboard text={myself}>
+              <Button variant="contained" color="primary" fullWidth startIcon={<Assignment fontSize="large"/>}>
+                Copy Your ID
+              </Button>
+            </CopyToClipboard>
+          </Grid>
+        </Grid>  
+        <Grid item xs={12} md={6}>
+            <Typography variant="h6" gutterBottom> Make A Call</Typography>
+            <TextField label="ID to CallS" value={idToCall} onChange={(e)=>setIdToCall(e.target.value)} fullWidth/>
+            {callAccepted && !callEnded ? (
+              <Button variant="contained" color="secondary" startIcon={<PhoneDisabled fontSize="large"/>} fullWidth onClick={leaveCall} sx={{mt:3}}>
+                Hang Up
+              </Button>):(
+              <Button variant="contained" color="primary" startIcon={<Phone fontSize="large"/>} fullWidth onClick={()=>callUser(idToCall)} sx={{mt:3}}>
+                Call
+              </Button>
+              )}       
+              </Grid>
+      </form>
+      {children}
+      </Paper>
+    </Container>
   )
 }
 
--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { Grid, Typography, Paper } from "@mui/material";
+// import {makeStyles} from "@mui/styles"
+
+import { SocketContext } from "./ProvideContext";
 
 const VideoPlayer = () => {
+  const { name, callAccepted, myVideo, userVideo, callEnded, stream, call } =
+    useContext(SocketContext);
   return (
-    <div>VideoPlayer</div>
-  )
-}
+    <Grid container>
+      {stream && (
+        <Paper>
+          <Grid item xs={12} md={6}>
+            <Typography variant="h5" gutterBottom>
+              {name || "Name"}{" "}
+            </Typography>
+            <video playsInline muted ref={myVideo} autoPlay className={null} />
+          </Grid>
+        </Paper>
+      )}
+      {callAccepted && !callEnded && (
+        <Paper>
+          <Grid item xs={12} md={6}>
+            <Typography variant="h5" gutterBottom>
+              {call.name || "Name"}{" "}
+            </Typography>
+            <video playsInline ref={userVideo} autoPlay className={null} />
+          </Grid>
+        </Paper>
+      )}
+    </Grid>
+  );
+};
 
-export default VideoPlayer
+export default VideoPlayer;
